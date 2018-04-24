fcf3a7001d9ba28066bc867bd36ea1d295fa97f7
Unknown <mmpcp23@gmail.com>
2018-04-24 15:30:55 +0100

    Map

--- Relogio/Apps/Navigation App/animation (Navigation App).js
+++ Relogio/Apps/Navigation App/animation (Navigation App).js
@@ -1,5 +1,5 @@
-slider.scrollTop = 372;
-anchors = [0, 186, 372, 560, 750];
+slider.scrollTop = 335;
+anchors = [ 0, 165, 335, 505, 750];
 
 function saveNavInfo(a){
     localStorage.setItem("label", a);
@@ -7,18 +7,18 @@ function saveNavInfo(a){
 
 function updateProgress() {
     if (slider.scrollTop <= 80) {
-        document.getElementById("Progress_Circles_Navigation").src = "../Health App/Images/progress1.png";
+        document.getElementById("Progress_Circles_Navigation").src = "Images/progress1.png";
     }
     if (slider.scrollTop > 96 && slider.scrollTop <= 276) {
-        document.getElementById("Progress_Circles_Navigation").src = "../Health App/Images/progress2.png";
+        document.getElementById("Progress_Circles_Navigation").src = "Images/progress2.png";
     }
     if (slider.scrollTop > 276 && slider.scrollTop <= 450) {
-        document.getElementById("Progress_Circles_Navigation").src = "../Health App/Images/progress3.png";
+        document.getElementById("Progress_Circles_Navigation").src = "Images/progress3.png";
     }
     if (slider.scrollTop > 450 && slider.scrollTop <= 650) {
-        document.getElementById("Progress_Circles_Navigation").src = "../Health App/Images/progress4.png";
+        document.getElementById("Progress_Circles_Navigation").src = "Images/progress4.png";
     }
     if (slider.scrollTop > 650) {
-        document.getElementById("Progress_Circles_Navigation").src = "../Health App/Images/progress5.png";
+        document.getElementById("Progress_Circles_Navigation").src = "Images/progress5.png";
     }
 }
