1db1d1d64a41993ecd2f223fb78ef0f7b1f089de
Unknown <miguel.valerio@tecnico.ulisboa.pt>
2018-04-25 00:29:35 +0100

    pequenas alteracoes

--- Relogio/Apps/Navigation App/animation (Friends).js
+++ Relogio/Apps/Navigation App/animation (Friends).js
@@ -13,13 +13,11 @@ function showContacts(startRow) {
   var contacts = JSON.parse(sessionStorage.getItem("contacts"));
   contacts.sort();
 
-  anchors = [0];
-  var lastAnchor = anchors[0];
+  anchors = [];
   var contact_Table = document.getElementById("Table");
 
   for (var i = 0; i < contacts.length; i++) {
-    lastAnchor += 170;
-    anchors.push(lastAnchor);
+    anchors.push(i*165);
 
     contact_Table.insertRow(i+startRow).innerHTML = element1 + JSON.stringify(contacts[i]) + element2 + contacts[i][0] + element3 + contacts[i] + element4;
   }
