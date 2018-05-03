47e9ac62a2d1ff08302ed4fc7c0b4490255953c8
Unknown <mmpcp23@gmail.com>
2018-05-03 16:15:42 +0100

    Perfection

--- Relogio/Apps/Navigation App/animation (Navigation App Map).js
+++ Relogio/Apps/Navigation App/animation (Navigation App Map).js
@@ -4,7 +4,8 @@ map.src = "Images/Map/map.png";
 var marker = new Image();
 marker.src = "Images/Map/map-marker.png";
 
-var zoom = 1;
+var zoom   = 3.5;
+var target = zoom;
 
 var ctx = document.getElementById("Map").getContext("2d");
 
@@ -17,22 +18,37 @@ var ty = 0;
 var dist = 0;
 
 function drawMap(){
+  setInterval(drawMap,1000);
+
+  if(zoom != target){
+    changeZoom(0.05);
+
+    if(Math.abs(zoom - target) > 0.05){
+      setInterval(drawMap,50);
+    }else {
+      zoom = target;
+    }
+  }
+
   var mapW = 210 * zoom;
   var mapH = mapW;
 
   var mapX = px - mapW/2;
   var mapY = py - mapH/2;
 
+  ctx.clearRect(0,0,210,210);
   ctx.drawImage(map, mapX, mapY, mapW, mapH,
                   0,    0, 210, 210);
 
-  ctx.drawImage(marker,0,0,64,64,tx - mapX - 20,ty - mapY - 40,40,40);
-
-  setInterval(drawMap,500);
+  ctx.drawImage(marker,0,0,64,64,(tx - mapX) / zoom - 20,(ty - mapY) / zoom - 40,40,40);
 }
 
 function setZoom(amount){
   zoom = amount;
+}
+
+function changeZoom(rate){
+  zoom += (target - zoom) * rate;
 }
 
 function getRandomArbitrary(min, max) {
@@ -74,18 +90,26 @@ function markerPosition() {
 
         case "1":
           tg = "WC";
+          tx = 321;
+          ty = 274;
         break;
 
         case "2":
           tg = "Palcos";
+          tx = 554;
+          ty = 335;
         break;
 
         case "3":
           tg = "Bebidas";
+          tx = 550;
+          ty = 420;
         break;
 
         case "4":
           tg = "Comida";
+          tx = 584;
+          ty = 267;
         break;
 
         case "5":
@@ -94,14 +118,16 @@ function markerPosition() {
 
         case "6":
           tg = "Saidas";
+          tx = 644;
+          ty = 328;
         break;
     }
 
     document.getElementById("Target").innerHTML   = tg;
     document.getElementById("Distance").innerHTML = dist + " m";
 
-    setZoom(1);
     drawMap();
+    target = 1;
 }
 
 // updateLookAt --> Function that update the vison cone's angle
