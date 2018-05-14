d10b4b968b06a909bd95caa8d039a8bc8fcf4943
Unknown <miguel.valerio@tecnico.ulisboa.pt>
2018-05-14 23:20:38 +0100

    mudar tamanho da caixa de mensagem conforme o que tem escrito --> DONE

--- Relogio/Apps/Contacts App/Chat/animation (Chat).js
+++ Relogio/Apps/Contacts App/Chat/animation (Chat).js
@@ -1,17 +1,29 @@
+var maxPerLine = 9;
+var nLines;
+var count;
+
 function fixMessage(message) {
-  var count = 0;
   var words = message.split(" ");
   var newMessage = "";
+
+  nLines = 1;
+  count  = 0;
 
   for (var w of words) {
     count += w.length;
 
-    if (count >= 10) {
-      count = w.length;
-      newMessage += "<br>";
+    if (count > maxPerLine) {
+      nLines++;
+      if (w.length > maxPerLine) {
+        newMessage += (" " + w.slice(0, maxPerLine) + "<br> " + w.slice(maxPerLine));
+        count = w.length - maxPerLine;
+      } else {
+        newMessage += "<br> " + w;
+        count = w.length;
+      }
+    } else {
+      newMessage += " " + w;
     }
-
-    newMessage += " " + w;
   }
 
   return newMessage;
@@ -21,27 +33,46 @@ function showMessages() {
   var chatTable = document.getElementById("Table");
   var friend    = sessionStorage.getItem("friend");
   var messages  = JSON.parse(sessionStorage.getItem("chat" + friend));
-  var element1  = "<span class=";
-  var element2  = " draggable='false'>";
-  var element3  = "</span>";
-  var spanClass;
+
+  var image1 = "<img class='";
+  var image2 = "' draggable='false' style='height:"; // height: (20*nLines + 10)px;
+  var image3 = "px; width:"; // width: (19 + w)px
+  var image4 = "px; left:";  // left: 5px / (150-w)px --> received / send
+  var image5 = "px' src='../Images/";
+  var image6 = "'>";
+  var span1  = "<span class='";
+  var span2  = "' draggable='false' style='margin-top:-";  // margin-top: -(20*nLines + 10)px;
+  var span3  = "px; left:";
+  var span4  = "px'>";
+  var span5  = "</span>";
+
+  var m;
+  var w;
+  var image;
+  var span;
 
   document.title += " " + friend;
   document.getElementById("FriendName").innerHTML = friend;
 
-  for (var i = 0; i < messages.length; i++) {
-    var m = fixMessage(messages[i][1]);
+  if (messages != null) {
+    for (var i = 0; i < messages.length; i++) {
+      m = fixMessage(messages[i][1]);
 
-    if (messages[i][0] == 0) { // received
-        spanClass = "'receivedMessage'";
-    } else { //send
-        spanClass = "'sendMessage'";
+      w = (nLines == 1 ? 19+11.25*count : 120);
+
+      if (messages[i][0] == 0) { // received
+        image = image1 + "receivedTemplate" + image2 + (20*nLines + 10) + image3 + w + image4 + 5 + image5 + "received background.png" + image6;
+        span  = span1 + "receivedMessage" + span2 + (20*nLines + 10) + span3 + 5 + span4 + m + span5;
+      } else { // send
+        image = image1 + "sendTemplate" + image2 + (20*nLines + 10) + image3 + w + image4 + (150-w) + image5 + "send background.png" + image6;
+        span  = span1 + "sendMessage" + span2 + (20*nLines + 10) + span3 + (150-w) + span4 + m + span5;
+      }
+
+      chatTable.insertRow(i).innerHTML = image + span;
     }
 
-    chatTable.insertRow(i).innerHTML = element1 + spanClass + element2 + m + element3;
+    slider.scrollTop = messages.length * 120;
   }
-
-  slider.scrollTop = messages.length * 150;
 }
 
 function anchor2closest() { }
